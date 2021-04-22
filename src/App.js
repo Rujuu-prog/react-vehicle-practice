f5c45ac911824e2f1a91ae1fa77e2eb9c914ba23
Rujuu-prog <n.ujiie2118@gmail.com>
2021-04-22 22:02:19 +0900

    add files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import styles from './App.module.css';
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import Auth from './components/Auth';
+import MainPage from './components/MainPage';
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
+    <div className={styles.app__root}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Auth} />
+          <Route exact path="/vehicle" component={MainPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
